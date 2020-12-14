1198b09e65b94fca5f4d1ecbaacc38b8e5fa9445
shanyul <l>
2020-12-14 17:33:26 +0800

    初始化Gin

---
+++ go.mod
@@ -0,0 +1,27 @@
+module learngo
+
+go 1.15
+
+require (
+	github.com/Bowery/prompt v0.0.0-20190916142128-fa8279994f75 // indirect
+	github.com/dchest/safefile v0.0.0-20151022103144-855e8d98f185 // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kardianos/govendor v1.0.9 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/ugorji/go v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20201208171446-5f87f3452ae9 // indirect
+	golang.org/x/net v0.0.0-20201207224615-747e23833adb
+	golang.org/x/sys v0.0.0-20201211090839-8ad439b19e0f // indirect
+	golang.org/x/tools v0.0.0-20201211185031-d93e913c1a58 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ pkg/setting/setting.go
@@ -0,0 +1,63 @@
+package setting
+
+import (
+	"log"
+	"time"
+
+	"gopkg.in/ini.v1"
+)
+
+
+var (
+	// Cfg 配置文件
+	Cfg *ini.File
+	// RunMode 运行模式
+	RunMode string
+	// HTTPPort 端口号
+	HTTPPort int
+	// ReadTimeout 读取超时时间
+	ReadTimeout time.Duration
+	// WaitTimeout 等待超时时间
+	WaitTimeout time.Duration
+	// PageSize 分页大小
+	PageSize int
+	// JwtSecret jwt密匙
+	JwtSecret string
+)
+
+func init() {
+	var err error
+	Cfg, err = ini.Load("conf/app.ini")
+	if err != nil {
+		log.Fatalf("Fail to parse 'conf/app.ini': %v", err)
+	}
+	LoadBase()
+	LoadServer()
+	LoadApp()
+}
+
+// LoadBase 加载基础配置
+func LoadBase() {
+	RunMode = Cfg.Section("").Key("RUN_MODE").MustString("debug")
+}
+
+// LoadServer 加载服务配置
+func LoadServer() {
+	sec, err := Cfg.GetSection("server")
+	if err != nil {
+		log.Fatalf("Fail to get section 'server': %v", err)
+	}
+	HTTPPort = sec.Key("HTTP_PORT").MustInt(8000)
+	ReadTimeout = time.Duration(sec.Key("READ_TIMEOUT").MustInt(60)) * time.Second
+	WaitTimeout = time.Duration(sec.Key("Wait_TIMEOUT").MustInt(60)) * time.Second
+}
+
+// LoadApp 加载App配置
+func LoadApp() {
+	sec, err := Cfg.GetSection("app")
+	if err != nil {
+		log.Fatalf("Fail to get section 'app': %v", err)
+	}
+	PageSize = sec.Key("PAGE_SIZE").MustInt(10)
+	JwtSecret = sec.Key("JWT_SECRET").MustString("!@65375374$@")
+}
