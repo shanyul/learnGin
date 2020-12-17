0164a46c389aca315e025dc3129f935ccab3fddd
shanyuliang <1021870536@qq.com>
2020-12-17 22:22:31 +0800

    20201217晚上

--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -1,9 +1,12 @@
 package v1
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/unknwon/com"
+	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+	"github.com/unknwon/com"
 
 	"learngo/models"
 	"learngo/pkg/e"
@@ -28,26 +31,80 @@ func GetTags(c *gin.Context) {
 		state = com.StrTo(arg).MustInt()
 		maps["state"] = state
 	}
-	
+
 	code := e.SUCCESS
 
 	data["list"] = models.GetTags(util.GetPage(c), setting.PageSize, maps)
 	data["total"] = models.GetTagTotal(maps)
 
 	c.JSON(http.StatusOK, gin.H{
-        "code" : code,
-        "msg" : e.GetMsg(code),
-        "data" : data,
-    })
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
 }
 
 // AddTag 添加标签
 func AddTag(c *gin.Context) {
-	/*name := c.Query("name")
+	name := c.Query("name")
 	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
 	createdBy := c.Query("created_by")
 
-	valid := validation*/
+	_tag := models.Tag{
+		Name:  name,
+		State:  state,
+		CreatedBy: createdBy,
+	}
+
+	rules := govalidator.MapData{
+		"name": []string{"required", "min:1", "max:40"},
+		"created_by":  []string{"required", "min:40"},
+		"state":  []string{"required"},
+	}
+
+	// 2. 定制错误消息
+	messages := govalidator.MapData{
+		"name": []string{
+			"required:标题为必填项",
+			"min:标题长度需大于 1",
+			"max:标题长度需小于 40",
+		},
+		"created_by": []string{
+			"required:创建人为必填项",
+			"min:长度需大于 40",
+		},
+		"state": []string{
+			"required:状态为必填项",
+		},
+	}
+
+	// 配置初始化
+	opts := govalidator.Options{
+		Data:          &_tag,
+		Rules:         rules,
+		TagIdentifier: "valid", // struct 标签标识符
+		Messages:      messages,
+	}
+
+	errors := govalidator.New(opts).ValidateStruct()
+
+	if len(errors) != 0 {
+		log.Fatal(errors)
+	}
+
+	code := e.INVALID_PARAMS
+	if !models.ExistTagByName(name) {
+		code = e.SUCCESS
+		models.AddTag(name, state, createdBy)
+	} else {
+		code = e.ERROR_EXIST_TAG
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 }
 
 // EditTag 修改标签
